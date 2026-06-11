196eb4f17559f2ade536c1e55c106d003d5b3262
ginnaaph <161983228+ginnaaph@users.noreply.github.com>
2026-06-11 05:59:42 -0700

    fix:  hooks/use-app-fonts.ts  to handle front loading errrors to prevent splash/render deadlock

--- hooks/use-app-fonts.ts
+++ hooks/use-app-fonts.ts
@@ -3,13 +3,12 @@ import { RobotoSlab_700Bold } from "@expo-google-fonts/roboto-slab";
 import { useFonts } from "expo-font";
 
 export function useAppFonts() {
-  const [fontsLoaded] = useFonts({
+  const [fontsLoaded, fontError] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     RobotoSlab_700Bold,
   });
-
   return {
-    fontsLoaded,
+    fontsLoaded: fontsLoaded || Boolean(fontError),
   };
 }
