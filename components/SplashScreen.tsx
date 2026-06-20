346d7892db617f5b2001101ab28cebb1e281379b
ginnaaph <161983228+ginnaaph@users.noreply.github.com>
2026-06-20 14:09:36 -0700

    fix: make profile page scrollable

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -8,7 +8,7 @@ type SplashScreenProps = {
 
 export function SplashScreen({ onGifLoad }: SplashScreenProps) {
   return (
-    <View className="flex-1 items-center justify-center bg-[#F5F0E8]">
+    <View className="flex-1 items-center justify-center">
       <Image
         source={images.bestListSplash}
         className="h-full w-full"
